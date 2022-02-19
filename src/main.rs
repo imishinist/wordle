7f58ea90308a4125e4dd7f45c30af330ad94d649
Taisuke Miyazaki <imishinist@gmail.com>
2022-02-20 04:11:20 +0900

    add char frequency analyse

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use std::collections::HashMap;
 use std::env;
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, Read};
 
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
 #[derive(Debug)]
 struct CharPosition {
@@ -86,7 +86,7 @@ impl Filter {
 
 #[cfg(test)]
 mod tests {
-    use crate::{CharPosition, Filter};
+    use crate::{CharFreq, CharPosition, Filter};
 
     #[test]
     fn filter_test() {
@@ -139,18 +139,26 @@ mod tests {
         assert!(!filter.accept("dense"));
         assert!(filter.accept("doree"));
     }
-}
 
-#[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
-struct Args {
-    target: Option<String>,
+    #[test]
+    fn char_freq_test() {
+        let mut char_freq = CharFreq::new();
 
-    #[clap(short, long)]
-    ignore_chars: Option<String>,
-
-    #[clap(short, long)]
-    different_positions: Option<Vec<String>>,
+        char_freq.add_char('a');
+        char_freq.add_char('a');
+        char_freq.add_char('b');
+        char_freq.add_char('c');
+        char_freq.add_char('\n');
+        char_freq.add_char('-');
+        assert_eq!(
+            char_freq
+                .to_vec()
+                .into_iter()
+                .filter(|(_, count)| *count > 0)
+                .collect::<Vec<(char, usize)>>(),
+            vec![('a', 2), ('b', 1), ('c', 1),]
+        );
+    }
 }
 
 fn parse_char_position(target: String) -> Vec<CharPosition> {
@@ -198,31 +206,113 @@ fn parse_different_positions(targets: Vec<String>) -> Vec<CharPosition> {
     ret
 }
 
-fn main() -> std::io::Result<()> {
-    let args = Args::parse();
-    let char_position = match args.target {
-        Some(t) => parse_char_position(t),
-        None => Vec::new(),
-    };
-    let ignore_chars = match args.ignore_chars {
-        Some(t) => parse_ignore_chars(t),
-        None => Vec::new(),
-    };
-    let not_match_char_position = match args.different_positions {
-        Some(t) => parse_different_positions(t),
-        None => Vec::new(),
-    };
-    let filter = Filter::new(ignore_chars, char_position, not_match_char_position);
+struct CharFreq {
+    inner: Vec<usize>,
+}
 
+impl CharFreq {
+    fn new() -> Self {
+        let inner = (0..26).map(|_| 0usize).collect();
+        Self { inner }
+    }
+
+    #[inline]
+    fn add_char(&mut self, c: char) {
+        if !c.is_ascii_alphabetic() {
+            return;
+        }
+        let c = c.to_ascii_lowercase() as usize - 'a' as usize;
+
+        let entry = unsafe { self.inner.get_unchecked_mut(c) };
+        *entry += 1;
+    }
+
+    #[inline]
+    fn to_vec(&self) -> Vec<(char, usize)> {
+        let mut v = self.inner.iter().enumerate().collect::<Vec<_>>();
+        v.sort_by(|(c1, &count1), (c2, &count2)| {
+            count1
+                .cmp(&count2)
+                .then_with(|| c1.cmp(c2).reverse())
+                .reverse()
+        });
+
+        v.iter()
+            .map(|(c, count)| ((*c as u8 + b'a') as char, **count))
+            .collect()
+    }
+}
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+#[clap(propagate_version = true)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Grep {
+        target: Option<String>,
+
+        #[clap(short, long)]
+        ignore_chars: Option<String>,
+
+        #[clap(short, long)]
+        different_positions: Option<Vec<String>>,
+    },
+    Analyse {},
+}
+
+fn main() -> std::io::Result<()> {
+    let cli = Cli::parse();
     let dict_path = env::var("DICT_PATH").unwrap_or_else(|_| "/usr/share/dict/words".into());
-    let file = File::open(dict_path)?;
-    let lines = BufReader::new(file).lines();
-    for line in lines {
-        match line {
-            Ok(line) if filter.accept(line.to_lowercase().as_str()) => {
-                println!("{}", line);
+
+    match &cli.command {
+        Commands::Grep {
+            target,
+            ignore_chars,
+            different_positions,
+        } => {
+            let char_position = match target {
+                Some(t) => parse_char_position(t.to_string()),
+                None => Vec::new(),
+            };
+            let ignore_chars = match ignore_chars {
+                Some(t) => parse_ignore_chars(t.to_string()),
+                None => Vec::new(),
+            };
+            let not_match_char_position = match different_positions {
+                Some(t) => parse_different_positions(t.clone()),
+                None => Vec::new(),
+            };
+            let filter = Filter::new(ignore_chars, char_position, not_match_char_position);
+
+            let file = File::open(dict_path)?;
+            let lines = BufReader::new(file).lines();
+            for line in lines {
+                match line {
+                    Ok(line) if filter.accept(line.to_lowercase().as_str()) => {
+                        println!("{}", line);
+                    }
+                    _ => continue,
+                }
             }
-            _ => continue,
+        }
+        Commands::Analyse {} => {
+            let mut file = File::open(dict_path)?;
+            let mut buffer = String::new();
+            file.read_to_string(&mut buffer)?;
+
+            let mut char_freq = CharFreq::new();
+            for c in buffer.chars() {
+                char_freq.add_char(c);
+            }
+
+            for (c, count) in char_freq.to_vec() {
+                println!("{}:{}", c, count);
+            }
         }
     }
 
