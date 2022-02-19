f6c099676d5eaf8c97a3b3175e2911f4ca255ffd
Taisuke Miyazaki <imishinist@gmail.com>
2022-02-20 06:40:50 +0900

    sort by char frequency score

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-use std::collections::HashMap;
+use bit_set::BitSet;
+use std::cmp::Ordering;
+use std::collections::{BinaryHeap, HashMap};
 use std::env;
 use std::fs::File;
-use std::io::{BufRead, BufReader, BufWriter, Read, stdout, Write};
+use std::io::{stdout, BufRead, BufReader, BufWriter, Read, Write};
+use std::path::Path;
 
 use clap::{Parser, Subcommand};
+use regex::Regex;
 
 #[derive(Debug)]
 struct CharPosition {
@@ -86,7 +90,8 @@ impl Filter {
 
 #[cfg(test)]
 mod tests {
-    use crate::{CharFreq, CharPosition, Filter};
+    use crate::{CharFreq, CharPosition, Filter, WordScore};
+    use std::collections::BinaryHeap;
 
     #[test]
     fn filter_test() {
@@ -159,6 +164,46 @@ mod tests {
             vec![('a', 2), ('b', 1), ('c', 1),]
         );
     }
+
+    #[test]
+    fn word_score_test() {
+        let mut char_freq = CharFreq::new();
+        char_freq.add_char('a');
+        char_freq.add_char('a');
+        char_freq.add_char('a');
+        char_freq.add_char('b');
+        char_freq.add_char('c');
+        let word_score = WordScore::new("aaaaaaabc".to_string(), &char_freq);
+        assert_eq!(word_score.score, 3 + 1 + 1);
+    }
+
+    #[test]
+    fn word_score_order() {
+        let mut heap = BinaryHeap::new();
+
+        let mut char_freq = CharFreq::new();
+        char_freq.add_char('a');
+        char_freq.add_char('a');
+        char_freq.add_char('a');
+        char_freq.add_char('b');
+        char_freq.add_char('c');
+
+        heap.push(WordScore::new("abc".to_string(), &char_freq));
+        heap.push(WordScore::new("cba".to_string(), &char_freq));
+        heap.push(WordScore::new("bcd".to_string(), &char_freq));
+
+        let ws = heap.pop().unwrap();
+        assert_eq!(ws.score, 5);
+        assert_eq!(ws.word, "abc".to_string());
+        let ws = heap.pop().unwrap();
+        assert_eq!(ws.score, 5);
+        assert_eq!(ws.word, "cba".to_string());
+        let ws = heap.pop().unwrap();
+        assert_eq!(ws.score, 2);
+        assert_eq!(ws.word, "bcd".to_string());
+
+        assert_eq!(heap.pop(), None);
+    }
 }
 
 fn parse_char_position(target: String) -> Vec<CharPosition> {
@@ -206,6 +251,7 @@ fn parse_different_positions(targets: Vec<String>) -> Vec<CharPosition> {
     ret
 }
 
+#[derive(Debug, Eq, PartialEq)]
 struct CharFreq {
     inner: Vec<usize>,
 }
@@ -228,6 +274,12 @@ impl CharFreq {
     }
 
     #[inline]
+    fn get_freq(&self, c: char) -> usize {
+        let c = c.to_ascii_lowercase() as usize - 'a' as usize;
+        unsafe { *self.inner.get_unchecked(c) }
+    }
+
+    #[inline]
     fn to_vec(&self) -> Vec<(char, usize)> {
         let mut v = self.inner.iter().enumerate().collect::<Vec<_>>();
         v.sort_by(|(c1, &count1), (c2, &count2)| {
@@ -240,6 +292,79 @@ impl CharFreq {
         v.iter()
             .map(|(c, count)| ((*c as u8 + b'a') as char, **count))
             .collect()
+    }
+
+    fn from_file<P: AsRef<Path>>(path: P) -> Self {
+        let file = File::open(path).unwrap();
+        let lines = BufReader::new(file).lines();
+
+        let re = Regex::new(r"^([a-z]):(\d+)$").unwrap();
+
+        let mut char_freq = Self::new();
+        for line in lines.flatten() {
+            for cap in re.captures_iter(line.as_str()) {
+                let char = cap.get(1).map(|c| c.as_str()).expect("invalid format");
+                let count = cap
+                    .get(2)
+                    .map(|c| c.as_str().parse::<usize>())
+                    .expect("invalid format")
+                    .expect("invalid format");
+                let c = char.as_bytes()[0] as usize - 'a' as usize;
+                let entry = unsafe { char_freq.inner.get_unchecked_mut(c) };
+                *entry = count;
+            }
+        }
+        char_freq
+    }
+}
+
+#[derive(Debug, Eq, PartialEq)]
+struct WordScore<'a> {
+    word: String,
+    score: usize,
+
+    freqs: &'a CharFreq,
+}
+
+impl<'a> WordScore<'a> {
+    fn new(word: String, char_freq: &'a CharFreq) -> Self {
+        let mut ws = Self {
+            word,
+            score: 0,
+
+            freqs: char_freq,
+        };
+
+        ws.calc_score();
+        ws
+    }
+
+    fn calc_score(&mut self) {
+        let mut s = BitSet::with_capacity(26);
+        for c in self.word.chars() {
+            let c = c.to_ascii_lowercase();
+            let c = c as u8 - b'a';
+            s.insert(c as usize);
+        }
+
+        for x in s.iter() {
+            let freq = self.freqs.get_freq((x as u8 + b'a') as char);
+            self.score += freq;
+        }
+    }
+}
+
+impl<'a> PartialOrd for WordScore<'a> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl<'a> Ord for WordScore<'a> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.score
+            .cmp(&other.score)
+            .then_with(|| self.word.cmp(&other.word).reverse())
     }
 }
 
@@ -261,6 +386,9 @@ enum Commands {
 
         #[clap(short, long)]
         different_positions: Option<Vec<String>>,
+
+        #[clap(short, long)]
+        score_sort: Option<usize>,
     },
     Analyse {},
 }
@@ -268,12 +396,21 @@ enum Commands {
 fn main() -> std::io::Result<()> {
     let cli = Cli::parse();
     let dict_path = env::var("DICT_PATH").unwrap_or_else(|_| "/usr/share/dict/words".into());
+    let char_freq_path = env::var("CHAR_FREQ_PATH").unwrap_or_else(|_| {
+        let current = env::current_dir().unwrap_or_else(|_| "/tmp".into());
+        current
+            .join("char.freq")
+            .into_os_string()
+            .into_string()
+            .unwrap()
+    });
 
     match &cli.command {
         Commands::Grep {
             target,
             ignore_chars,
             different_positions,
+            score_sort,
         } => {
             let char_position = match target {
                 Some(t) => parse_char_position(t.to_string()),
@@ -294,13 +431,37 @@ fn main() -> std::io::Result<()> {
 
             let out = stdout();
             let mut out = BufWriter::new(out.lock());
-            for line in lines {
-                match line {
-                    Ok(line) if filter.accept(line.to_lowercase().as_str()) => {
-                        out.write(line.as_bytes())?;
-                        out.write(b"\n")?;
+            if score_sort.is_some() {
+                let char_freq = CharFreq::from_file(char_freq_path);
+                let mut k = score_sort.unwrap_or_else(|| 100) as isize;
+
+                let mut heap = BinaryHeap::with_capacity(k as usize);
+                for line in lines {
+                    match line {
+                        Ok(line) if filter.accept(line.to_lowercase().as_str()) => {
+                            heap.push(WordScore::new(line, &char_freq));
+                        }
+                        _ => continue,
                     }
-                    _ => continue,
+                }
+
+                while let Some(ws) = heap.pop() {
+                    if k <= 0 {
+                        break;
+                    }
+                    k -= 1;
+
+                    println!("{}", ws.word);
+                }
+            } else {
+                for line in lines {
+                    match line {
+                        Ok(line) if filter.accept(line.to_lowercase().as_str()) => {
+                            out.write_all(line.as_bytes())?;
+                            out.write_all(b"\n")?;
+                        }
+                        _ => continue,
+                    }
                 }
             }
         }
@@ -314,10 +475,13 @@ fn main() -> std::io::Result<()> {
                 char_freq.add_char(c);
             }
 
-            let out = stdout();
-            let mut out = BufWriter::new(out.lock());
+            let out = File::options()
+                .create(true)
+                .write(true)
+                .open(char_freq_path)?;
+            let mut out = BufWriter::new(out);
             for (c, count) in char_freq.to_vec() {
-                out.write(format!("{}:{}\n", c, count).as_bytes())?;
+                out.write_all(format!("{}:{}\n", c, count).as_bytes())?;
             }
         }
     }
